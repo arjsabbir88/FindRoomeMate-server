5fddec4579635fec7f78458e97c3b19842109150
arjsabbir88 <arjsabbir88@gmail.com>
2025-05-22 18:18:59 +0600

    conneted with mongodb and server fetch data form clint side successfully

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
 const port = process.env.port || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@arjsabbir.9jwrtji.mongodb.net/?retryWrites=true&w=majority&appName=arjSabbir`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const addListingCollection = client.db('addListing').collection('roommates-listing');
+
+        app.get('/roommates-listing', async (req, res) => {
+            const result = await addListingCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.post('/roommates-listing', async (req, res) => {
+            const newRoommate = req.body;
+            console.log(newRoommate)
+            const result = await addListingCollection.insertOne(newRoommate);
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send("finding roomMate in the server");
@@ -14,9 +62,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log("server is running on port ", port)
 })
-
-
-
-// u2GWEIw9isGM0IYJ
-// // arjsabbir88
-// mongodb+srv://arjsabbir88:1EkJOcW3zKC3rVgP@arjsabbir.9jwrtji.mongodb.net/?retryWrites=true&w=majority&appName=arjSabbir
